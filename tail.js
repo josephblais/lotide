cb681ec3efda8a58e03a34fd0dc7f4cef39f0fee
Joseph Blais <josephpierreblais@gmail.com>
2021-02-01 15:39:53 -0500

    outputs an array minus the first element

---
+++ tail.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
+};
+
+const tail = (array) => {
+  return array.slice(1);
+};
+
+console.log(tail([1]));
