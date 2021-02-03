534c725b74763810907d758bbb5ba72afe6f0384
Joseph Blais <josephpierreblais@gmail.com>
2021-02-03 17:28:15 -0500

    created program that outputs an object containing arrays of index positions of each letter for a given string

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,5 +12,4 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   eqArrays(array1, array2) === true ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`❌❌❌ Assertion Failed: ${array1} != ${array2}`);
-
 };
--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,32 @@
-const letterPositions = function (sentence) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  eqArrays(array1, array2) === true ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`❌❌❌ Assertion Failed: ${array1} != ${array2}`);
+};
+
+
+const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
-      (results[sentence[i]]).push(i);
-    } else if (sentence[i] !== " ") {
-      results[sentence[i]] = [];
-      (results[sentence[i]]).push(i);
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        (results[sentence[i]]).push(i);
+      } else if (!results[sentence[i]]) {
+        results[sentence[i]] = [i];
+      }
     }
   }
   return results;
-}
+};
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
