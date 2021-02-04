6737e997d7f06742d4e7afa42a0b7598ec781c17
Joseph Blais <josephpierreblais@gmail.com>
2021-02-03 20:26:35 -0500

    created working eqObjects() function that tests the equality of two objects and returns a boolean value

--- eqObjects.js
+++ eqObjects.js
@@ -2,30 +2,57 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
 };
 
-// Returns true if both objects have identical keys w/ identical values
-// otherwise returns false
-const eqObjects = function(object1, object2) {
-  //compare # of keys
-  console.log(`Object1 length: ${Object.keys(object1).length}`);
-  console.log(`Object2 length: ${Object.keys(object2).length}`);
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   }
-
-  //value for each key in object1 === value for same key in object2
-  for (const key of object1) {
-    if (object1[key] !== object2[key]) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
+  return true;
+};
 
+
+// Returns true if both objects have identical keys w/ identical values
+// otherwise returns false
+const eqObjects = function(object1, object2) {
+  //compare # of keys -- OK
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    console.log(false);
+    process.exit();
+  }
+
+  for (const key in object1) {
+    //value for each key in object1 === value for same key in object2 -- OK
+    if (object1[key] !== object2[key]) {
+      console.log(false);
+      process.exit();
+    }
+    
+    //support array value comparisons:
+    if (Array.isArray(object1[key])) {
+      //make sure identical keys both contain arrays
+      if (Array.isArray(object2[key]) === false) {
+        console.log(false);
+        process.exit();
+      }
+      // test arrays with eqArrays();
+      if (eqArrays(object1[key], object2[key]) === false) {
+        console.log(false);
+        process.exit();
+      }
+    }
+  }
+  console.log(true);
 };
 
 //TESTS
-const ab = { a: "1", b: "2"};
-const ba = {b: "2", a: "1"};
+const ab = { a: ["1","2"], b: "2"};
+const ba = {b: "2", a: ["1","2"]};
 
-// eqObjects(ab,ba); //=>TRUE
+eqObjects(ba,ba); //=>TRUE
 
 const abc = {a: "1", b: "2", c: "3"};
 eqObjects(ab, abc);//=>FALSE
