6929cda2b0d70a9e868a16cff34d3b1011e263cd
Joseph Blais <josephpierreblais@gmail.com>
2021-02-08 13:37:18 -0500

    refactored middle.js to import modules

--- middle.js
+++ middle.js
@@ -1,16 +1,7 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i] || array1.length !== array2.length) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 
-const assertArraysEqual = function(array1, array2) {
-  eqArrays(array1, array2) === true ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`❌❌❌ Assertion Failed: ${array1} != ${array2}`);
-};
 
 const middle = function(array) {
   const emptyArray = [];
@@ -46,7 +37,7 @@ const getTwoMiddleElements = function(input) {
 
 const test1 = [1,2,3,4,5];
 const test2 = ["a","b","c","d"];
-console.log(assertArraysEqual(middle(test1),[3]));
-console.log(assertArraysEqual(middle(test2),["b","c"]));
+assertArraysEqual(middle(test1),[3]);
+assertArraysEqual(middle(test2),["b","c"]);
 // console.log(middle(test1));
 // console.log(middle(test2));
